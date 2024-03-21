35d66a85ffa54a691fea88b4b286b615aba8b3d2
Conor Stuart-Roe <conorstuartroe@gmail.com>
2024-03-21 19:26:19 -0400

    Register models with admin

--- spaced_repetition/admin.py
+++ spaced_repetition/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from spaced_repetition.models.document import Document
+from spaced_repetition.models.language import Language
+from spaced_repetition.models.lemma import Lemma
+from spaced_repetition.models.lemma_add import LemmaAdd
+from spaced_repetition.models.sentence import Sentence
+from spaced_repetition.models.trial import Trial
+from spaced_repetition.models.word_in_sentence import WordInSentence
 
-# Register your models here.
+
+admin.site.register(Document)
+admin.site.register(Language)
+admin.site.register(Lemma)
+admin.site.register(LemmaAdd)
+admin.site.register(Sentence)
+admin.site.register(Trial)
+admin.site.register(WordInSentence)
