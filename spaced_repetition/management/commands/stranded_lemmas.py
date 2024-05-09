fd521daa765d140bf7d26a95faf7bee6be992563
Conor Stuart-Roe <conorstuartroe@gmail.com>
2024-05-08 22:09:16 -0400

    Delete stranded lemmas

--- spaced_repetition/management/commands/stranded_lemmas.py
+++ spaced_repetition/management/commands/stranded_lemmas.py
@@ -22,12 +22,29 @@ class Command(BaseCommand):
             if lemma.id not in lemma_ids:
                 print(f"{lemma.language.name} lemma {lemma.citation_form} {json.dumps(lemma.translation)}")
 
+                all_trials = []
+                all_lemma_adds = []
+
                 for lemma_add in LemmaAdd.objects.filter(lemma_id=lemma.id):
+                    all_lemma_adds.append(lemma_add)
+
                     trials = list(Trial.objects.filter(lemma_add_id=lemma_add.id).order_by('time_created'))
+                    all_trials += trials
 
                     if not trials:
                         print(f"{lemma_add.user.username} has never trialed this lemma")
                     else:
                         print(f"This lemma comes due for {lemma_add.user.username} "
                               f"on {trials[-1].due_date()} with trial type {TRIAL_TYPE_CYCLE[trials[-1].trial_type]}")
+
+                print(f"Deleting this lemma will delete {len(all_trials)} trials and {len(all_lemma_adds)} lemma adds.")
+                delete = input("Delete? ")
+                if delete.lower().strip() == "y":
+                    for trial in all_trials:
+                        trial.delete()
+                    for lemma_add in all_lemma_adds:
+                        lemma_add.delete()
+                    lemma.delete()
+
+                print()
 
