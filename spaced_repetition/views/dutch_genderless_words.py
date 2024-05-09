ba8525ba57c26fda5e80fb3d90173a2ce7002db0
Conor Stuart-Roe <conorstuartroe@gmail.com>
2024-05-09 00:22:15 -0400

    Only set genders for added words

--- spaced_repetition/views/dutch_genderless_words.py
+++ spaced_repetition/views/dutch_genderless_words.py
@@ -2,7 +2,7 @@ from django.views import View
 from django.http import HttpRequest, JsonResponse
 from .ajax_utils import admin_only
 from spaced_repetition.models.language import LANGUAGE_IDS
-from spaced_repetition.models.lemma import Lemma
+from spaced_repetition.models.lemma_add import LemmaAdd
 
 
 DUTCH_LANGUAGE_ID = LANGUAGE_IDS["Dutch"]
@@ -10,13 +10,19 @@ DUTCH_LANGUAGE_ID = LANGUAGE_IDS["Dutch"]
 
 class DutchGenderlessWordsView(View):
     @admin_only
-    def get(self, _request: HttpRequest):
-        lemmas = Lemma.objects.filter(language_id=DUTCH_LANGUAGE_ID).order_by("id")
+    def get(self, request: HttpRequest):
+        lemma_adds = (
+                     LemmaAdd.objects
+                     .filter(user_id=request.user.id)
+                     .select_related('lemma')
+                     .filter(lemma__language_id=DUTCH_LANGUAGE_ID)
+                     .order_by("id")
+        )
 
         lemmas_with_unspecified_gender = [
-            lemma
-            for lemma in lemmas
-            if lemma.metadata_value("gender") is None
+            lemma_add.lemma
+            for lemma_add in lemma_adds
+            if lemma_add.lemma.metadata_value("gender") is None
         ]
 
         return JsonResponse(
