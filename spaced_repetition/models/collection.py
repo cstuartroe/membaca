b79f3a1c7875d08d69b32163d748c2f389c11ce2
Conor Stuart-Roe <conorstuartroe@gmail.com>
2024-07-24 00:41:21 -0400

    Create __str__ method for Collection

--- spaced_repetition/models/collection.py
+++ spaced_repetition/models/collection.py
@@ -6,6 +6,9 @@ class Collection(models.Model):
     title = models.CharField(max_length=64)
     language = models.ForeignKey(Language, on_delete=models.PROTECT)
 
+    def __str__(self):
+        return f"{self.title} ({self.language.name})"
+
     def to_json(self):
         return {
             "id": self.id,
